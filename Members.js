73987e9db559a824727324d7ad2b9a3c4141bbdb
Zorica Jelic <zoricajelicst@gmail.com>
2019-06-28 22:59:50 +0200

    resto

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'john@gmail.com',
+        status: 'activ'
+    },
+    {
+        id: 2,
+        name: 'John Doe1',
+        email: 'john1@gmail.com',
+        status: 'inactiv'
+    },
+    {
+        id: 3,
+        name: 'John Doe2',
+        email: 'john2@gmail.com',
+        status: 'activ'
+    },
+
+]
+module.exports = members;
